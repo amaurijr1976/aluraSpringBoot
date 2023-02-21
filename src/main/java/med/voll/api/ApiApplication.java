37663a71ac4f97699e335c503fa461c8041498f3
AMAURI ANTUNES PEREIRA JUNIOR <amaurijr1976@hotmail.com>
2023-02-21 18:35:24 -0300

    subida

--- src/main/java/med/voll/api/ApiApplication.java
+++ src/main/java/med/voll/api/ApiApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class ApiApplication {
 
 	public static void main(String[] args) {
+		//teste
 		SpringApplication.run(ApiApplication.class, args);
 	}
 
